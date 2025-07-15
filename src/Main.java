4480c2eedabb28f74686354bc74144d20b19bd35
Michael Stepannikov <veryoldMIX@gmail.com>
2025-07-15 20:49:13 +0300

    Парсинг введенного с консоли пути к файлу. Проверка существования файла и корректности введенного пути + подсчет количества правильно введенных путей

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int fileCount = 0;
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный путь неверен.");
+                continue;
+            }
+            fileCount++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + fileCount);
+        }
     }
 }

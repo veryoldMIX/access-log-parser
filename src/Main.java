e96a386ff2381323d210eab803b7b0a028f66f19
Michael Stepannikov <veryoldMIX@gmail.com>
2025-08-18 21:34:49 +0300

    Added line-by-line reading of the file, exception handling, counting the total number of lines in the file, length of the longest string, and length of the shortest string. Added a RuntimeException when encountering a string longer than 1024 characters.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int fileCount = 0;
+        String path;
         while (true) {
             System.out.print("Введите путь к файлу: ");
-            String path = new Scanner(System.in).nextLine();
+            path = new Scanner(System.in).nextLine();
 
             File file = new File(path);
             boolean fileExists = file.exists();
-
             boolean isDirectory = file.isDirectory();
 
             if (!fileExists || isDirectory) {
@@ -20,6 +20,37 @@ public class Main {
             fileCount++;
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + fileCount);
+
+            FileReader fileReader = null;
+            try {
+                fileReader = new FileReader(path);
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = "";
+            int maxLength = 0;
+            int minLength = Integer.MAX_VALUE;
+            int lineCount = 0;
+            while (true) {
+                try {
+                    if ((line = reader.readLine()) == null) break;
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                lineCount++;
+                int currentLength = line.length();
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                }
+                if (currentLength < minLength && currentLength > 0) {
+                    minLength = currentLength;
+                }
+                if (currentLength > 1024) throw new RuntimeException("Длина строки более 1024 символов");
+            }
+            System.out.println("Количество строк: " + lineCount);
+            System.out.println("Максимальная длина строки: " + maxLength);
+            System.out.println("Минимальная длина строки: " + minLength);
         }
     }
 }
